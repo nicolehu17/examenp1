a15131dae7cfdbe8a433f2131ed632e1c5b07f92
unknown <nicolehu@uees.edu.ec>
2024-06-18 08:22:20 -0500

    examen parcial 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './shared/inicio/inicio.component';
+import { NosotrosComponent } from './shared/nosotros/nosotros.component';
+import { NoticiasComponent } from './shared/noticias/noticias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/Inicio', pathMatch: 'full' },
+  { path: 'Inicio', component: InicioComponent },
+  { path: 'Nosotros', component: NosotrosComponent },
+  { path: 'Noticias', component: NoticiasComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
